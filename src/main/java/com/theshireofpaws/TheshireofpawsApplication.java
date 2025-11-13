8cceaf44b467b708e8e0abc903000c88b1cf2b58
Andrea <aoliverar.96@gmail.com>
2025-11-13 20:43:21 +0100

    feat(entities):add entities folder with entities adn enums

--- src/main/java/com/theshireofpaws/TheshireofpawsApplication.java
+++ src/main/java/com/theshireofpaws/TheshireofpawsApplication.java
@@ -1,4 +1,4 @@
-package com.theshireofpaws.theshireofpaws;
+package com.theshireofpaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
