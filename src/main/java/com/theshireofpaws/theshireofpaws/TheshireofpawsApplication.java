ed93493c3e368c17c905fa1e2e0db702491f7646
Andrea <aoliverar.96@gmail.com>
2025-11-13 19:38:37 +0100

    initial springboot project setup

---
+++ src/main/java/com/theshireofpaws/theshireofpaws/TheshireofpawsApplication.java
@@ -0,0 +1,13 @@
+package com.theshireofpaws.theshireofpaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheshireofpawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheshireofpawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theshireofpaws/theshireofpaws/TheshireofpawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theshireofpaws.theshireofpaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheshireofpawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
